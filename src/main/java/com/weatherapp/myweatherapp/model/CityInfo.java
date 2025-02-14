d9e20d9faf0602737c22a90b98328a68243bb31c
kiranm0303 <kiranm0303@gmail.com>
2025-02-14 02:38:10 +0000

    Compare daylight between two cities

---
+++ src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
@@ -0,0 +1,128 @@
+package com.weatherapp.myweatherapp.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CityInfo {
+
+  @JsonProperty("address")
+  private String address;
+
+  @JsonProperty("description")
+  private String description;
+
+  @JsonProperty("currentConditions")
+  private CurrentConditions currentConditions;
+
+  @JsonProperty("days")
+  private List<Days> days;
+
+  // Getter methods
+  public CurrentConditions getCurrentConditions() {
+      return currentConditions;
+  }
+
+  public String getAddress() {
+      return address;
+  }
+
+  public String getDescription() {
+      return description;
+  }
+
+  public List<Days> getDays() {
+      return days;
+  }
+
+  // *** Change: Make CurrentConditions PUBLIC ***
+  public static class CurrentConditions {
+    @JsonProperty("temp")
+    private String currentTemperature;
+
+    @JsonProperty("sunrise")
+    private String sunrise;
+
+    @JsonProperty("sunset")
+    private String sunset;
+
+    @JsonProperty("feelslike")
+    private String feelslike;
+
+    @JsonProperty("humidity")
+    private String humidity;
+
+    @JsonProperty("conditions")
+    private String conditions;
+
+    // Getter methods for sunrise and sunset
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public String getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public String getFeelsLike() {
+        return feelslike;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+  }
+
+  public static class Days {
+    @JsonProperty("datetime")
+    private String date;
+
+    @JsonProperty("temp")
+    private String currentTemperature;
+
+    @JsonProperty("tempmax")
+    private String maxTemperature;
+
+    @JsonProperty("tempmin")
+    private String minTemperature;
+
+    @JsonProperty("conditions")
+    private String conditions;
+
+    @JsonProperty("description")
+    private String description;
+
+    // Getter methods
+    public String getDate() {
+        return date;
+    }
+
+    public String getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public String getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public String getMinTemperature() {
+        return minTemperature;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+  }
+}
